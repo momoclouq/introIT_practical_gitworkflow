ee161bb0e623d923616def2951a0d685abb78890
momoclouq <phamhoangminh050899@gmail.com>
2020-08-21 09:35:10 +0700

    fixed all the erros baby :)

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 P = float(input('initial amount '))
-if P <= 0
-    
-r = float(input('interest rate '))
-n = float(input('number of times interest is compounded '))
-#t = float(input('number of years '))
-print('final amount', P*(1+r/n)**n*t)
+if P >= 0:
+    r = float(input('interest rate '))
+    n = float(input('number of times interest is compounded '))
+    t = float(input('number of years '))
+    print('final amount', P*(1+r/n)**n*t)
